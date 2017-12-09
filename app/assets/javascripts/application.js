91bfa389676bf2dfe1801b32ff0d4f4a355776f3
lauren inacio <lauren_f23@yahoo.com>
2017-12-09 17:14:06 -0500

    added partial file and rendered it in the application view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
