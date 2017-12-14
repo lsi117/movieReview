7cbd4ce9d58c2475c30fa6ad34a11c45e80fcb68
lauren inacio <lauren_f23@yahoo.com>
2017-12-14 14:55:48 -0500

    working on trying to deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+    #Needed for Devise
+  config.action_mailer.default_url_options = { host: 'https://movie-review-app-lsi.herokuapp.com/' }
+
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
